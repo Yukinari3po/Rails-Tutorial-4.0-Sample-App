8e4e5cb159f4292f6a422a1af285c834bea7923d
Yukinari3po <Yukinari3po@gmail.com>
2014-10-07 16:44:00 +0900

    Add the failure test code for Help page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,19 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  describe "GET /static_pages" do
+  describe "Home page" do
 
     it "shoud have the content 'Sample App'" do
 	visit '/static_pages/home'
 	expect(page).to have_content('Sample App')
+    end
+  end
 
+  describe "Help Page" do
+
+    it "should have the content 'Help'" do
+	visit '/static_pages/home'
+        expect(page).to have_content('help')
     end
   end
 end
