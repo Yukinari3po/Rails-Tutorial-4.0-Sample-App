6f27b8c0d2853924da051a2bab7d586ba7db2f24
Yukinari3po <Yukinari3po@gmail.com>
2014-10-07 20:24:59 +0900

    Edit Test file for the website title and add the title tag to the home.html.erb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,8 +5,13 @@ describe "StaticPages" do
   describe "Home page" do
 
     it "shoud have the content 'Sample App'" do
+  visit '/static_pages/home'
+  expect(page).to have_content('Sample App')
+    end
+
+    it "shoud have the content 'Home'" do
 	visit '/static_pages/home'
-	expect(page).to have_content('Sample App')
+	expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
     end
   end
 
@@ -15,6 +20,24 @@ describe "StaticPages" do
     it "should have the content 'Help'" do
 	visit '/static_pages/help'
         expect(page).to have_content('Help')
+    end
+
+    it "should have the content 'Help'" do
+  visit '/static_pages/help'
+        expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do 
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
     end
   end
 end
