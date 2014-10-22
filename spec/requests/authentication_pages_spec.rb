53be8489a71e95ce5a7bdeb3140b5eb0270ac068
Yukinari3po <Yukinari3po@gmail.com>
2014-10-22 10:45:14 +0900

    Add tests for Sign in page(8.1.2).

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,5 +9,26 @@ describe "Authentication" do
 
 		it { should have_content('Sign in')}
 		it { should have_title('Sign in')}
+
+		describe "sign in" do
+			before { click_button "Sign in" }
+
+			it { should have_title('Sign in') }
+			it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+			decscribe "with valid information" do
+				let(:user) { FactoryGirl.create(:user) }
+
+				before do
+					fill_in "Email", 	with: user.email.upcase
+					fill_in "Password", with: user.password
+					click_button "Sign in"
+				end
+
+				it { should have_title(user.name) }
+				it { should have_link('Profile', href: user_path(user)) }
+				it { should have_link('Sign out', href: signout_path) }
+				it { shoule_not have_link('Sign in', href: signin_path) }
+		end
 	end
 end
