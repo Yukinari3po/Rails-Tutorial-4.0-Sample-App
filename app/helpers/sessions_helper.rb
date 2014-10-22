4ce735ea4294b258f8602550b899d4573481d463
Yukinari3po <Yukinari3po@gmail.com>
2014-10-22 13:55:16 +0900

    Add cookie checking method 'current_user' (8.2.3).

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,4 +5,18 @@ module SessionsHelper
 		user.update_attribute(:remember_token, User.encrypt(remember_token))
 		self.current_user = user
 	end
+
+	def current_user=(user) # 演算子のオーバーライドのようなもの
+		@current_user = user
+	end
+
+
+	# current_user
+	# 
+	# #ユーザーのブラウザに記録されたトークンを受け取り、暗号化してremember_tokenに代入する。
+	# remember_tokenを使用してユーザー一覧から検索し、@current_userが未定義の時にのみ代入する。
+	def current_user
+		remember_token = User.encrypt(cookies[:remember_token])　		
+		@current_user ||= User.find_by(remember_token: remember_token)  
+	end
 end
