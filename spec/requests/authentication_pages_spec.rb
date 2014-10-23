e24c29c438cbb7703312a66dac53526f66042c55
Yukinari3po <Yukinari3po@gmail.com>
2014-10-23 14:12:42 +0900

    Add authorization for disable editing another user's page.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -63,5 +63,23 @@ describe "Authentication" do
 				end
 			end
 		end
+
+		describe "as wrong user" do
+			# メールアドレスのみ異なるユーザーを用意している。
+			let(:user) { FactoryGirl.create(:user) }
+			let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+			before { sign_in user, no_capybara: true }
+
+			describe "submitting a GET request to the Users#edit astion" do
+				before { get edit_user_path(wrong_user) }
+				specify { expect(response.body).not_to match(full_title('Edit user')) }
+				specify { expect(response).to redirect_to(root_url) }
+			end
+
+			describe "submitting a PATCH request to the Users#update action" do
+				before { patch user_path(wrong_user) }
+				specify { expect(response).to redirect_to(root_path) }
+			end
+		end
 	end
 end
